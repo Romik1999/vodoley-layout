9d56779ea8f05048effb2b0438a5d5a37b04e58e
Romik1999 <usankin.roma@gmail.com>
2023-04-05 23:09:43 +0400

    work on adapt catalog items, filter

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,28 @@ document.addEventListener('DOMContentLoaded', () => {
             prevEl: '.banners-slider__arrow--prev',
         }
     });
+
+    let filterButton = document.querySelector('.catalog__button');
+    let filterMobile = document.querySelector('.catalog-side');
+    let filterCloser = document.querySelector('.catalog-side__close');
+
+    filterButton.addEventListener('click', () => {
+        if (filterButton.classList.contains('active')) {
+            filterButton.classList.remove('active')
+            filterMobile.classList.remove('catalog-side--active')
+            document.body.style.overflow = 'visible'
+        } else {
+            filterButton.classList.add('active')
+            filterMobile.classList.add('catalog-side--active')
+            document.body.style.overflow = 'hidden'
+        }
+    });
+
+    filterCloser.addEventListener('click', () => {
+        if (filterButton.classList.contains('active')) {
+            filterButton.classList.remove('active')
+            filterMobile.classList.remove('catalog-side--active')
+            document.body.style.overflow = 'visible'
+        }
+    });
 })
